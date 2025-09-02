b203ed8e97c2a331e32dbe8c3787dc77d7b5f774
metth-ijsje <matthijs.both@gmail.com>
2025-09-02 12:19:03 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        CutebotPro.pwmCruiseControl(0, 50)
+        CutebotPro.pwmCruiseControl(30, 100)
     }
     if (receivedNumber == 2) {
-        CutebotPro.pwmCruiseControl(50, 0)
+        CutebotPro.pwmCruiseControl(100, 30)
     }
     if (receivedNumber == 0) {
         CutebotPro.pwmCruiseControl(100, 100)
@@ -11,21 +11,26 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 3) {
         CutebotPro.fullAstern()
     }
+    if (receivedNumber == 4) {
+        CutebotPro.cruiseControl(0, 0, CutebotProSpeedUnits.Cms)
+        radio.setGroup(2)
+    }
     if (receivedNumber == 0) {
         t_start = input.runningTime()
     }
-    if (receivedNumber == geheimpje) {
-        basic.showIcon(IconNames.Yes)
-        music.play(music.tonePlayable(131, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-    } else {
-        music.stopAllSounds()
-    }
-    if (true) {
-    	
-    }
 })
+radio.onReceivedString(function (receivedString) {
+    t_Next = input.runningTime()
+    T_tussen = t_Next - t_prev
+    t_prev = t_Next
+    radio.sendString("C")
+    radio.setGroup(1)
+    basic.showString(receivedString)
+})
+let T_tussen = 0
+let t_Next = 0
 let t_start = 0
-let geheimpje = 0
+let t_prev = 0
 basic.showLeds(`
     # # . # #
     # # . # #
@@ -35,7 +40,7 @@ basic.showLeds(`
     `)
 radio.setGroup(1)
 CutebotPro.colorLight(CutebotProRGBLight.RGBL, 0x7f00ff)
-geheimpje = randint(0, 20)
+t_prev = 0
 basic.forever(function () {
-    radio.sendNumber(geheimpje)
+    radio.sendNumber(0)
 })
